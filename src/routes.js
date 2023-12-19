88aa175633898f21998a29a14087ade933eaa0d8
João Vitor Dias Évora <42285911+joaovitorevora@users.noreply.github.com>
2023-12-19 10:51:01 -0300

    Criando pag favoritos

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Home from './pages/Home';
 import Filme from './pages/Filme';
+import Favoritos from './pages/favoritos';
 
 import Erro from './pages/Erro';
 
@@ -12,9 +13,9 @@ function RoutesApp() {
         <BrowserRouter>
             <Header/>
             <Routes>
-                <Route path='/' element={< Home/> } />
-                <Route path='/filme/:id' element={< Filme/> } />
-
+                <Route path='/' element={<Home/> } />
+                <Route path='/filme/:id' element={<Filme/> } />
+                <Route path='/favoritos' element={<Favoritos/> }/>
 
 
                 <Route path="*" element={<Erro/>}/>
